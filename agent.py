5df081e5cc76c8a7c4775041b599304523c81b51
Faiz Surani <faiz.surani@gmail.com>
2022-03-28 21:19:19 +0200

    Implement model logic

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import cast
 
 from mesa import Agent
 
@@ -15,6 +16,7 @@ class InfectionState(int, Enum):
 
 
 class CovidAgent(Agent):
+    model: CovidModel
     state: InfectionState
 
     def __init__(
@@ -24,4 +26,32 @@ class CovidAgent(Agent):
         self.state = initial_state
 
     def step(self) -> None:
-        pass
+        # Since we handle infection of susceptibles on the infector side, we don't
+        # need to implement any logic here for susceptible agents
+        if self.state == InfectionState.INFECTED:
+            # Either the agent recovers or spreads the virus with some probability
+            if self.random.random() < self.model.recovery_prob:
+                self.state = InfectionState.SUSCEPTIBLE
+            else:
+                self.infect_neighbors()
+
+    def infect_neighbors(self) -> None:
+        """
+        Infect susceptible neighbors with probability model.infection_prob
+        """
+        for neighbor in self.neighbors:
+            if (
+                neighbor.state == InfectionState.SUSCEPTIBLE
+                and self.random.random() < self.model.infection_prob
+            ):
+                neighbor.state = InfectionState.INFECTED
+
+    @property
+    def neighbors(self) -> list["CovidAgent"]:
+        # Cast does nothing, just keeps the type-checker happy
+        return cast(
+            list[CovidAgent],
+            self.model.grid.get_cell_list_contents(
+                self.model.grid.get_neighbors(self.unique_id)
+            ),
+        )
--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import cast
 
 from mesa import Model
 from mesa.time import RandomActivation
@@ -6,22 +6,21 @@ from mesa.space import NetworkGrid
 from mesa.datacollection import DataCollector
 import networkx as nx
 
-from project.agent import InfectionState, CovidAgent
+from agent import InfectionState, CovidAgent
 
 
 class CovidModel(Model):
     network: nx.Graph
+    grid: NetworkGrid
     infection_prob: float
     recovery_prob: float
 
     def __init__(
         self,
         num_nodes=500,
-        avg_degree=3,
+        avg_degree=10,
         infection_prob=0.1,
         recovery_prob=0.01,
-        *args: Any,
-        **kwargs: Any
     ):
         """
         Initializes the COVID model.
@@ -29,15 +28,16 @@ class CovidModel(Model):
         :param num_nodes: The number of nodes in the model
         :param avg_degree: The average degree of nodes in the model
         :param infection_prob: Probability of an agent infecting a connected agent during a single time step
-        :param recovery_chance: The chance that an infected node recovers during a single time step
+        :param recovery_prob: The chance that an infected node recovers during a single time step
         """
 
-        super().__init__(*args, **kwargs)
+        super().__init__()
         self.recovery_prob = recovery_prob
         self.infection_prob = infection_prob
 
         self.schedule = RandomActivation(self)
-        self.network = nx.erdos_renyi_graph(num_nodes, avg_degree)
+        edge_probability = avg_degree / num_nodes
+        self.network = nx.erdos_renyi_graph(num_nodes, edge_probability)
         self.grid = NetworkGrid(self.network)
 
         self.datacollector = DataCollector(
@@ -49,7 +49,7 @@ class CovidModel(Model):
 
         # Initialize agents
         for i, node in enumerate(self.network.nodes):
-            # Infect only one node
+            # Infect only one node to start with
             infection_state = (
                 InfectionState.SUSCEPTIBLE if i > 0 else InfectionState.INFECTED
             )
@@ -57,10 +57,9 @@ class CovidModel(Model):
             self.schedule.add(agent)
             self.grid.place_agent(agent, node)
 
-        self.datacollector.collect(self)
-
     def step(self) -> None:
-        pass
+        self.datacollector.collect(self)
+        self.schedule.step()
 
     @property
     def num_susceptible(self) -> int:
@@ -71,5 +70,8 @@ class CovidModel(Model):
         return self.num_with_state(InfectionState.INFECTED)
 
     def num_with_state(self, state: InfectionState) -> int:
-        # TODO: Figure out how to implement this
-        pass
+        return sum(1 for agent in self.agents if agent.state == state)
+
+    @property
+    def agents(self) -> list[CovidAgent]:
+        return cast(list[CovidAgent], self.grid.get_all_cell_contents())
